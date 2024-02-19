f9fa48e416bc9c9829b041a29c8f769f22d8af4c
Christophe Blefari <christophe.blefari@gmail.com>
2024-02-19 16:47:49 +0100

    Add black / isort

--- ghost.py
+++ ghost.py
@@ -2,8 +2,8 @@ import urllib
 from datetime import datetime
 
 import dlt
-from dlt.sources.helpers import requests
 import jwt
+from dlt.sources.helpers import requests
 
 
 @dlt.source
@@ -12,20 +12,16 @@ def ghost_source(admin_api_key=dlt.secrets.value):
 
 
 def _get_jwt_token(admin_api_key):
-    id, secret = admin_api_key.split(':')
+    id, secret = admin_api_key.split(":")
 
     # Prepare header and payload
     iat = int(datetime.now().timestamp())
 
-    header = {'alg': 'HS256', 'typ': 'JWT', 'kid': id}
-    payload = {
-        'iat': iat,
-        'exp': iat + 5 * 60,
-        'aud': '/admin/'
-    }
+    header = {"alg": "HS256", "typ": "JWT", "kid": id}
+    payload = {"iat": iat, "exp": iat + 5 * 60, "aud": "/admin/"}
 
     # Create the token (including decoding secret)
-    return jwt.encode(payload, bytes.fromhex(secret), algorithm='HS256', headers=header)
+    return jwt.encode(payload, bytes.fromhex(secret), algorithm="HS256", headers=header)
 
 
 def _create_auth_headers(admin_api_key):
@@ -37,8 +33,8 @@ def _create_auth_headers(admin_api_key):
 
 @dlt.resource(table_name="activities", write_disposition="merge", primary_key="id")
 def get_activities(
-        admin_api_key=dlt.secrets.value,
-        created_at=dlt.sources.incremental("$.data.created_at"),
+    admin_api_key=dlt.secrets.value,
+    created_at=dlt.sources.incremental("$.data.created_at"),
 ):
     headers = _create_auth_headers(admin_api_key)
     base_url = "https://christopheblefari.ghost.io/ghost/api/admin/members/events"
@@ -72,13 +68,21 @@ def get_activities(
         for activity in activities:
             activity["id"] = activity["data"]["id"]
 
-            cursor = min([datetime.strptime(activity["data"]["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"), cursor])
+            cursor = min(
+                [
+                    datetime.strptime(
+                        activity["data"]["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                    ),
+                    cursor,
+                ]
+            )
             yield activity
 
-        print(created_at.start_value)
-        print(cursor)
-
-        if len(activities) < limit or (created_at.start_value is not None and cursor < created_at.start_value):
+        if len(activities) < limit or (
+            created_at.start_value is not None
+            and cursor
+            < datetime.strptime(created_at.start_value, "%Y-%m-%dT%H:%M:%S.%fZ")
+        ):
             keep_going = False
 
 
@@ -86,21 +90,25 @@ def remove_columns(doc):
     return {
         "id": doc["id"],
         "type": doc["type"],
-        "member_id": doc["data"]["member"].get("id") if "member" in doc["data"] else None,
+        "member_id": (
+            doc["data"]["member"].get("id") if "member" in doc["data"] else None
+        ),
         "created_at": doc["data"]["created_at"],
         "score": doc["data"]["score"] if "score" in doc else None,
         "post": doc["data"]["post"] if "post" in doc["data"] else None,
         "link": doc["data"]["link"] if "link" in doc["data"] else None,
-        "email_post_id": doc["data"]["email"]["post_id"] if "email" in doc["data"] else None,
+        "email_post_id": (
+            doc["data"]["email"]["post_id"] if "email" in doc["data"] else None
+        ),
     }
 
 
 if __name__ == "__main__":
     # configure the pipeline with your destination details
     pipeline = dlt.pipeline(
-        pipeline_name='ghostdb',
-        destination='duckdb',
-        dataset_name='ghost',
+        pipeline_name="ghostdb",
+        destination="duckdb",
+        dataset_name="ghost",
         # loader_file_format="parquet",
     )
 
